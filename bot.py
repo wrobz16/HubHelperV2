bbace824de8118433b4df0ae4e7038a210d654ad
wrobz16 <jasuwr@gmail.com>
2023-10-31 10:18:02 -0500

    Initial build

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import json
+
+# Load the JSON file
+with open("config.json", "r") as file:
+    config = json.load(file)
+
+PREFIX = config["prefix"]
+TOKEN = config["token"]
+
+
+# Initialize bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=PREFIX, intents=intents, case_insensitive=True)
+
+# When bot is loaded
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord.')
+
+# For messages
+@bot.event
+async def on_message(message):
+    print(f'[{message.guild.name} - #{message.channel.name} - {message.author.name}]: {message.content}')
+
+    await bot.process_commands(message)
+
+# Run the bot
+bot.run(TOKEN)
