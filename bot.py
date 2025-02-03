bec45496b4d83d7c8954213279cf8430a5fc4afc
wrobz16 <jasuwr@gmail.com>
2025-02-03 15:04:35 -0600

    added basic auto delete for pancake messages

--- bot.py
+++ bot.py
@@ -5,19 +5,19 @@ import json
 import openai
 import os
 
-# Test to see if Visual Studio Git is configured
-
 # Load configs
 with open("config.json", "r") as file:
     config = json.load(file)
 PREFIX = config["prefix"]
 TOKEN = config["token"]
+BOT_ID = config["bot_id"]
+MESSAGE_CHANNEL_ID = config["message_channel_id"]
+SONG_REQUEST_CHANNEL_ID = config["song_request_channel_id"]
+
 
 # Initialize bot
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, case_insensitive=True)
-
-# Set some other things
 
 # On ready
 @bot.event
@@ -25,7 +25,6 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord.')
 
 # Load commands
-
 async def load_extensions():
     for filename in os.listdir("./commands"):
         if filename.endswith(".py") and filename != "__init__.py":
@@ -34,6 +33,12 @@ async def load_extensions():
 # For messages
 @bot.event
 async def on_message(message):
+    # Bot message deletion
+    if message.author.id == BOT_ID and message.channel.id == MESSAGE_CHANNEL_ID:
+        await message.delete()
+        print(f'Deleted message from {message.author.name} in #{message.channel.name}')
+        await message.channel.send(f'Pancake song requests go in {bot.get_channel(SONG_REQUEST_CHANNEL_ID).mention} dumb fuck.')
+
     print(f'[{message.guild.name} - #{message.channel.name} - {message.author.name}]: {message.content}')
     await bot.process_commands(message)
 
@@ -43,4 +48,6 @@ async def main():
         await load_extensions()
         await bot.start(TOKEN)
 
+# Start bot
 asyncio.run(main())
+
